b5c4d8160a608e8a86d2131726f69a347a80da30
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-04-26 13:03:53 +0200

    Enhance README and backend setup: update project description, add CORS support, and improve contact form handling

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Better Call Buonuomo, Attorney assistance  (portfolio project)
 
-This project is about create a site that will look like a attorney real like site, with a close up to the better call saul ironic style.
+This project is about create a site that will look like a attorney real like site, with a close up to the better call saul ironic style with React.
 
 The site will be developed this way:
 
@@ -10,4 +10,4 @@ Homepage.jsx with home
 Contacts.jsx with a form to send requests of contact on backend
 AboutUs.jsx with info
 
-(import till now: Bootstrap)
+(import till now: Bootstrap, Express, Vite + React, CORS)
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const app = express()
+const cors = require("cors") /* cors import */
 const port = 3000
+
+app.use(cors()) /* avoid communication issues from backend and front end on the same machine */
+
+app.use(express.json()) /* body parser to read req.body converting data in a js object */
+
 
 app.get('/', (req, res) => {
   res.send('Benvenuto al server Better Caul Buonuomo')
 })
 
-app.post('/', (req, res) => {
-  res.send("This is a post request")
+app.post('/api/contact', (req, res) => {
+  const {Name, LastName, Email} = req.body
+
+  if(!Name || !LastName || !Email ){
+   return res.status(400).json({success: false, messagge:"All Data must be inserted"})
+  }
+
+
+  console.log("Recived Data", req.body);
+  res.status(200).json({success: true, message:"Contact Received"})
+  
 })
 
 app.listen(port, () => {
